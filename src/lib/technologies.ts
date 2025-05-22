463db3cb3baf23f071b0418e46f57ee8c6e5a247
set <lucameopou@gmail.com>
2025-05-22 23:21:45 +0200

    feat(technologies): add technologies list with icons for various programming tools

---
+++ src/lib/technologies.ts
@@ -0,0 +1,30 @@
+import { ArrowDownUp, type LucideIcon } from "lucide-react"
+import {
+  Code,
+  Coffee,
+  Database,
+  FileText,
+  Github,
+  Package,
+  ShipWheel,
+  Terminal,
+  Zap,
+} from "lucide-react"
+
+interface Technology {
+  name: string
+  icon: LucideIcon
+}
+
+export const technologies: Technology[] = [
+  { name: "Java", icon: Coffee },
+  { name: "SQL", icon: Database },
+  { name: "GitHub", icon: Github },
+  { name: "Paper", icon: FileText },
+  { name: "Velocity", icon: Zap },
+  { name: "Javalin", icon: ArrowDownUp },
+  { name: "Docker", icon: ShipWheel },
+  { name: "Ubuntu", icon: Terminal },
+  { name: "JetBrains IDEs", icon: Code },
+  { name: "Reposilite", icon: Package },
+]
