868d5601ca7a5b8f40e931dbe5eb6bd107992b90
set <lucameopou@gmail.com>
2025-05-23 00:13:17 +0200

    feat(vite): include package dependencies in Vite optimization configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 import tailwindcss from "@tailwindcss/vite"
+import pkg from "./package.json"
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  optimizeDeps: {
+    include: Object.keys(pkg.dependencies),
+  },
   build: {
     emptyOutDir: true,
     cssCodeSplit: true,
