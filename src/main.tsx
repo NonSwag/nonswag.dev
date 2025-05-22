1594c32ef9a21e9266bd85ba5321bb30e3854078
set <lucameopou@gmail.com>
2025-05-22 23:22:10 +0200

    feat(routing): implement routing with react-router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react"
+import { createRoot } from "react-dom/client"
+import "./index.css"
+import App from "./App.tsx"
+import { BrowserRouter, Route, Routes } from "react-router"
+import NotFoundPage from "./components/not-found.tsx"
+import TechStack from "./components/tech_stack.tsx"
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/tech_stack.txt" element={<TechStack />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
 )
